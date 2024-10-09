6dc75d5cb0b915208a0ed61e7e1b8851e37ed2c1
Raymond Terry <raymondt1234@gmail.com>
2024-10-09 15:42:00 -0700

    add javascript to submit profile form and get card data to generate cards from a template

--- scripts/index.js
+++ scripts/index.js
@@ -28,7 +28,31 @@ const profileEditButton = document.querySelector(".profile__edit-btn");
 const editModal = document.querySelector("#edit-profile-modal");
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
 
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+
+const modalName = editModal.querySelector("#profile-name-input");
+const modalDescription = editModal.querySelector("#profile-description-input");
+const profileFormElement = editModal.querySelector(".modal__form");
+
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
+
+  const cardNameElement = cardElement.querySelector(".card__title");
+  const cardImageElement = cardElement.querySelector(".card__image");
+  cardNameElement.textContent = data.name;
+  cardImageElement.src = data.link;
+  cardImageElement.alt = data.name;
+
+  return cardElement;
+}
+
 function openModal () {
+  modalName.value = profileName.textContent;
+  modalDescription.value = profileDescription.textContent;
   editModal.classList.add("modal_opened");
 };
 
@@ -36,6 +60,21 @@ function closeModal () {
   editModal.classList.remove("modal_opened");
 };
 
-profileEditButton.addEventListener("click", openModal);
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
 
+  profileName.textContent = modalName.value;
+  profileDescription.textContent = modalDescription.value;
+
+  closeModal();
+}
+
+profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+
+}
