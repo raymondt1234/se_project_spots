247a997adbaa46ca5c0640cada9099dc948188bb
Raymond Terry <raymondt1234@gmail.com>
2025-02-01 13:37:25 -0700

    feat: connect project to a database via API

---
+++ src/utils/helpers.js
@@ -0,0 +1,9 @@
+function setButtonText(btn, isLoading, defaultText = "Save", loadingText = "Saving...") {
+  if (isLoading) {
+    btn.textContent = loadingText;
+  } else {
+    btn.textContent = defaultText;
+  }
+}
+
+export { setButtonText };
