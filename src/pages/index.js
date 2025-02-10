2ba4b453951c6d428c484eef21c206323d54853b
Raymond Terry <raymondt1234@gmail.com>
2025-02-10 14:40:41 -0700

    fix: disable save button after submitting new Avatar picture and validate url

--- src/pages/index.js
+++ src/pages/index.js
@@ -198,6 +198,7 @@ function handleAvatarFormSubmit(event) {
     .then((data) => {
       profileAvatar.src = data.avatar;
 
+      disableButton(avatarSubmitBtn, true, settings);
       event.target.reset();
       closeModal(editAvatarModal);
     })
@@ -233,33 +234,6 @@ function handleNewPostFormSubmit(event) {
       setButtonText(submitBtn, false);
     });
 }
-
-function handleSubmit(request, event, loadingText = 'Saving...') {
-  // You need to prevent the default action in any submit handler
-   event.preventDefault();
-
-   // the button is always available inside `event` as `submitter`
-   const submitButton = event.submitter;
-   // fix the initial button text
-   const initialText = submitButton.textContent;
-   // change the button text before requesting
-   setButtonText(true, submitButton, initialText, loadingText);
-   // call the request function to be able to use the promise chain
-   request()
-     .then(() => {
-       // any form should be reset after a successful response
-       // evt.target is the form in any submit handler
-       event.target.reset();
-     })
-       // we need to catch possible errors
-       // console.error is used to handle errors if you don’t have any other ways for that
-     .catch(console.error)
-
-     // and in finally we need to stop loading
-     .finally(() => {
-       setButtonText(false, submitButton, initialText);
-     });
- }
 
 // Handles Delete Form Submission
 function handleDeleteSubmit(event) {
@@ -315,6 +289,14 @@ newPostButton.addEventListener("click", () => {
 deleteForm.addEventListener("submit", handleDeleteSubmit);
 
 profileAvatarButton.addEventListener("click", () => {
+  avatarLink.value = "";
+
+  resetValidation(
+    editAvatarModal,
+    [avatarLink],
+    settings
+  );
+
   openModal(editAvatarModal);
 });
 
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -19,68 +19,62 @@ class Api {
     return Promise.all([this.getInitialCards(), this.getUserInfo()]);
   }
 
-  // getInitialCards() {
-  //   this._request(`${this._baseUrl}/cards`, {
-  //     headers: this._headers,
-  //   });
-  // }
-
   getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
+    return this._request(`${this._baseUrl}/cards`, {
       headers: this._headers,
-    }).then(this._checkResponse);
+    });
   }
 
   getUserInfo() {
-    return fetch(`${this._baseUrl}/users/me`, {
+    return this._request(`${this._baseUrl}/users/me`, {
       headers: this._headers,
-    }).then(this._checkResponse);
+    });
   }
 
   editUserInfo({ name, about }) {
-    return fetch(`${this._baseUrl}/users/me`, {
+    return this._request(`${this._baseUrl}/users/me`, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({
         name,
         about,
       }),
-    }).then(this._checkResponse);
+    });
   }
 
   editUserAvatar({ avatar }) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
+    return this._request(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({
         avatar,
       }),
-    }).then(this._checkResponse);
+    });
   }
 
   addCard({ name, link }) {
-    return fetch(`${this._baseUrl}/cards`, {
+    return this._request(`${this._baseUrl}/cards`, {
       method: "POST",
       headers: this._headers,
       body: JSON.stringify({
         name,
         link,
       }),
-    }).then(this._checkResponse);
+    });
   }
 
   deleteCard(id) {
-    return fetch(`${this._baseUrl}/cards/${id}`, {
+    return this._request(`${this._baseUrl}/cards/${id}`, {
       method: "DELETE",
       headers: this._headers,
-    }).then(this._checkResponse);
+    });
   }
 
   updateLikes(id, isLiked) {
-    return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+    return this._request(`${this._baseUrl}/cards/${id}/likes`, {
       method: isLiked ? "DELETE" : "PUT",
       headers: this._headers,
-    }).then(this._checkResponse);
+    });
   }
 }
 
